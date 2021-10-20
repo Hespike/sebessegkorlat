9fdc5cbe329a7d56e1791313d58738f2a3231243
Sárdi András <sardi.andras@gmail.com>
2021-10-20 14:19:41 +0200

    Create  sebessegkorlat.cpp

---
+++ sebessegkorlat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int kijelez(int max) {
+    cout << "Maximalis sebesseg: " << max << endl;
+    return 0;
+}
+
+/*
+ #include <iostream>
+
+using namespace std;
+
+void kijelez(int speed) {
+    cout << "Maximalis sebesseg: " << speed << endl;
+}
+ */
